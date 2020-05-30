864c1296a8cdd28fa59ae34f72496493363e4993
Bill White <william.a.white.iii@zeptive.com>
2020-05-30 16:27:14 -0400

    test commit

--- z_functions.sh
+++ z_functions.sh
@@ -12,21 +12,41 @@
 
 # Since we require this file to be in the project directory, let us make
 # the bad assumption that pwd is the project root ZDIR.
-ZDIR=${PWD}
+if [ "${ZDIR}" == "" ]; then
+    ZDIR=${PWD}
+fi
 
 git config --global user.name "Bill White"
 git config --global user.email william.a.white.iii@zeptive.com
 
 #
+# run the Particle "make clean:" script
+#
+function z_make_clean() {
+    echo -n
+    #does not work
+    #/home/white3/.particle/toolchains/deviceOS/1.5.2/modules/argon/system-part1/makefile
+    #/home/white3/.particle/toolchains/deviceOS/1.5.2/modules/argon/user-part/makefile
+    #make -f '/home/white3/.particle/toolchains/buildscripts/1.9.2/Makefile' clean-all
+
+}
+
+#
 #commit all changes in the project directory
 #
 function z_git_add_commit_all() {
-    local answer
+    if [ "${ZDIR}" == "" ]; then
+	echo ZDIR is not set
+	return
+    fi
+    
     (cd $ZDIR
+     local answer
      echo -n "Enter the commit message: "
      read answer
+     z_make_clean
      git add .
-     git diff --cached
+     #git diff --cached
      git commit -a -m "${answer}"
     )
 }
