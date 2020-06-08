96f3d1193ab0367c8b7afeb60d487daab27b810a
Bill White <william.a.white.iii@zeptive.com>
2020-06-07 23:06:16 -0400

    Backup Sun Jun  7 23:06:14 EDT 2020

--- src/zbuild.h
+++ src/zbuild.h
@@ -1,5 +1,5 @@
 // This file generated autmatically - do not edit
 //
-const char BUILD_DATE[] = "Sun Jun  7 22:27:14 EDT 2020";
-const unsigned long BUILD_NUMBER = 1591583234;
-const unsigned int BUILD_RANDOM_NUMBER = 22812;
+const char BUILD_DATE[] = "Sun Jun  7 23:06:08 EDT 2020";
+const unsigned long BUILD_NUMBER = 1591585568;
+const unsigned int BUILD_RANDOM_NUMBER = 25880;
